824b2254c16f76f57331b1d1311f92d737191d1b
Duncan Townsend <duncant@mit.edu>
2013-05-22 09:48:51 -0400

    Made SecureRandom keep a cache of bits for efficiency

--- SecureRandom.py
+++ SecureRandom.py
@@ -4,6 +4,8 @@ class SecureRandom(random.Random):
     def __init__(self, paranoid=False):
         self._file = None
         self._paranoid = paranoid
+        self._cache = 0L
+        self._cache_len = 0L
         self.seed(None)
 
     def seed(self, ignore):
@@ -27,8 +29,15 @@ class SecureRandom(random.Random):
     def random(self):
         return abs(struct.unpack('l', self._file.read(4))[0])/(0.+(~(1<<31)))
     def getrandbits(self, n):
-        retval = 0L
-        bytes = int(math.ceil(n / 8.0))
+        bytes = max(int(math.ceil((1.0*n-self._cache_len) / 8.0)),0)
+
         for i in xrange(bytes):
-            retval |= struct.unpack('B', self._file.read(1))[0] << i*8
-        return retval >> (bytes * 8 - n)
+            self._cache <<= 8
+            self._cache |= struct.unpack('B', self._file.read(1))[0]
+            self._cache_len += 8
+
+        assert self._cache_len >= n
+        retval = ((1<<n) - 1) & self._cache
+        self._cache >>= n
+        self._cache_len -= n
+        return retval
