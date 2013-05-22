7bba9330df792af9073f1decc774f0f3bf4d0351
Duncan Townsend <duncant@mit.edu>
2013-05-22 11:48:33 -0400

    Rewrote SecureRandom in light of SystemRandom's superiority. It only handles /dev/random now.

--- SecureRandom.py
+++ SecureRandom.py
@@ -1,24 +1,27 @@
 import random, struct, math
 
+# TODO: re-implement random methods for correctness
 class SecureRandom(random.Random):
-    def __init__(self, paranoid=False):
+    """
+    A random number generator that gets its randomness from /dev/random.
+    If you want to get randomness from /dev/urandom, use random.SystemRandom instead.
+    """
+    def __init__(self):
         self._file = None
-        self._paranoid = paranoid
         self._cache = 0L
         self._cache_len = 0L
         self.seed(None)
 
     def seed(self, ignore):
         if self._file is None:
+            self._file = open('/dev/random', 'r')
+        else:
             try:
-                close(self._file)
+                self._file.close()
             except:
                 pass
-            if self._paranoid:
-                fname = '/dev/random'
-            else:
-                fname = '/dev/urandom'
-            self._file = open(fname, 'r')
+            self._file = None
+            self.seed(None)
 
     def getstate(self):
         return None
@@ -26,16 +29,41 @@ class SecureRandom(random.Random):
         pass
     def jumpahead(self, ignore):
         pass
+
     def random(self):
-        return abs(struct.unpack('l', self._file.read(4))[0])/(0.+(~(1<<31)))
+        bits_per_float = 53
+        randomness = self.getrandbits(bits_per_float)
+        return float(randomness) / (2**bits_per_float)
+
     def getrandbits(self, n):
-        bytes = max(int(math.ceil((1.0*n-self._cache_len) / 8.0)),0)
+        bytes = max(int(math.ceil((n-self._cache_len) / 8.0)),0)
 
-        for i in xrange(bytes):
-            self._cache <<= 8
-            self._cache |= struct.unpack('B', self._file.read(1))[0]
-            self._cache_len += 8
+        if bytes >= 8:
+            for _ in xrange(0,bytes,8):
+                self._cache <<= 8*8
+                self._cache |= struct.unpack('Q', self._file.read(8))[0]
+                self._cache_len += 8*8
+                bytes -= 8
+        if bytes >= 4:
+            for _ in xrange(0,bytes,4):
+                self._cache <<= 8*4
+                self._cache |= struct.unpack('L', self._file.read(4))[0]
+                self._cache_len += 8*4
+                bytes -= 4
+        if bytes >= 2:
+            for _ in xrange(0,bytes,2):
+                self._cache <<= 8*2
+                self._cache |= struct.unpack('H', self._file.read(2))[0]
+                self._cache_len += 8*2
+                bytes -= 2
+        if bytes >= 1:
+            for _ in xrange(0,bytes,1):
+                self._cache <<= 8*1
+                self._cache |= struct.unpack('B', self._file.read(1))[0]
+                self._cache_len += 8*1
+                bytes -= 1
 
+        assert bytes == 0
         assert self._cache_len >= n
         retval = ((1<<n) - 1) & self._cache
         self._cache >>= n
